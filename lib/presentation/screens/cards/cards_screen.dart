385fa06869fecb0aacec82a73e3db010528ae9d3
= <=>
2025-11-06 13:48:37 +0100

    Fix unused property.

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -198,8 +198,6 @@ class _CardType4 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
-    final colors = Theme.of(context).colorScheme;
-    
     return Card(
       clipBehavior: Clip.hardEdge,
       elevation: elevation,
