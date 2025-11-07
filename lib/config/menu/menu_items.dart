bc6ab190af55af71185b9e073f2354f5d4ac0fdd
= <=>
2025-11-07 10:35:32 +0100

    UI Screen switch, radio, check

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -45,4 +45,10 @@ const appMenuItems = <MenuItem>[
     link: '/animated', 
     icon: Icons.check_box_outline_blank_outlined,
   ),
+  MenuItem(
+    title: 'Ui Controls + Tiles', 
+    subTitle: 'Controles en Flutter', 
+    link: '/ui-controls', 
+    icon: Icons.car_rental_outlined,
+  ),
 ];
